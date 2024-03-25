df4171d31055514ce3955f59c58c0c463bbfac1d
Chan Yoon <psalms119165sin@gmail.com>
2024-03-25 14:37:12 +0900

    [Bronze II] Title: 대표값2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main()
+{
+	const int max{ 5 };
+
+	int Bucket[9]{};
+	int acc{}, input{};
+
+	for (int i = 0; i < max; i++) 
+	{
+		std::cin >> input;
+		
+		++Bucket[input / 10 - 1]; 
+	
+		acc += input;
+	}
+
+	std::cout << acc / max << std::endl;
+
+	for (int i = 0, j = 0; i < 9; i++) 
+	{
+		if (max / 2 < j + Bucket[i])
+		{
+			std::cout << (i + 1) * 10;
+			break;
+		}
+		else
+			j += Bucket[i];
+	}
+
+	return 0;
+}
