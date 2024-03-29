48b2435c61bad8b40e464ecdfbebfa0d750a66b5
Chan Yoon <psalms119165sin@gmail.com>
2024-03-29 12:31:00 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -0,0 +1,120 @@
+#include <iostream>
+
+struct NODE
+{
+	NODE(const int& value, NODE* link = nullptr)
+		: _value(value), _link(link) {};
+
+	const int _value{};
+	NODE* _link{};
+};
+
+class Queue
+{
+public:
+	Queue() {};
+	~Queue() { Clear(); };
+
+public:
+	void Enqueu(const int& value)
+	{
+		NODE* node{ new NODE{value} };
+
+		if (nullptr == node)
+			return;
+
+		if (Empty())
+		{
+			_front = _rear = node;
+		}
+		else
+		{
+			if (_front == _rear)
+				_front->_link = node;
+			else
+				_rear->_link = node;
+
+			_rear = node;
+		}
+	}
+
+	const int Dequeue()	
+	{
+		if (Empty())
+		{
+			return int{};
+		}
+		else
+		{
+			const int value{ _front->_value };
+
+			if (_front == _rear)
+			{
+				delete _front;
+				_front = _rear = nullptr;
+			}
+			else
+			{
+				NODE* link{ _front->_link };
+				delete _front;
+				_front = link;
+			}
+
+			return value;
+		}
+	}
+
+	const bool Empty() const
+	{
+		return nullptr == _front;
+	}
+
+private:
+	void Clear()
+	{
+		NODE* link{};
+
+		while (nullptr != _front)
+		{
+			link = _front->_link;
+			delete _front;
+			_front = link;
+		}
+
+		_front = _rear = nullptr;
+	}
+
+private:
+	NODE* _front{}, * _rear{};
+};
+
+int main()
+{
+	int n{}, k{};
+	Queue queue{};
+
+	std::cin >> n >> k;
+
+	for (int i = 1; i <= n; i++)
+		queue.Enqueu(i);
+
+	std::cout << "<";
+	while (!queue.Empty())
+	{
+		for (int i = 0; i < k; i++)
+		{
+			if (k - 1 == i)
+			{
+				std::cout << queue.Dequeue();
+
+				if (!queue.Empty())
+					std::cout << ", ";
+			}
+			else
+				queue.Enqueu(queue.Dequeue());
+		}
+	}
+	std::cout << ">";
+
+	return 0;
+}
