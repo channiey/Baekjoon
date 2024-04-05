097852044bd3579480a4f1a520e1436209f5376c
Chan Yoon <psalms119165sin@gmail.com>
2024-04-05 16:39:39 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 100 ms, Memory: 22844 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.cc
@@ -1,27 +1,49 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
+
+#define MAX 200000
+
+int Table[26][MAX + 1]{}; 
+// ex. Table[0][5] : 알파벳 a의, 문자열 5번까지 등장횟수
 
 int main()
 {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
 	string str;
 	char alphabet{};
-	int question{}, left{}, right{}, cnt{};
+	int question{}, left{}, right{};
 
 	cin >> str >> question;
-	
+
+	// Creater Prefix Sum
+	const int len{ int(str.length()) };
+	for (int i = 0; i < 26; i++)
+	{
+		if (char('a' + i) == str[0])
+			Table[i][0] = 1;
+
+		for (int j = 1; j < len; j++)
+		{
+			Table[i][j] = Table[i][j - 1];
+
+			// str의 j번째 문자열이 i인 경우
+			if (char('a' + i) == str[j])
+				++Table[i][j];
+		}
+	}
+
 	while (question--)
 	{
 		cin >> alphabet >> left >> right;
 
-		for (int i = left; i <= right; i++)
-		{
-			if (alphabet == str[i])
-				++cnt;
-		}
-		cout << cnt << "\n";
-		cnt = 0;
+		if (0 == left)
+			cout << Table[alphabet - 'a'][right] << "\n";
+		else
+			cout << Table[alphabet - 'a'][right] - Table[alphabet - 'a'][left - 1] << "\n";
 	}
 
 	return 0;
